8ac595917980695c41672b0d64f7fffc5e8a8a48
massstab <dave@xiala.net>
2022-12-06 12:33:55 +0100

    Day 6

---
+++ Day-06/06a-Tuning-Trouble.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Name:      massstab
+   Date:      06.12.22
+   Kurs:      Advent of Code
+   Topic:     Tuning Trouble a
+"""
+
+char_list = []
+
+with open("data/input.txt") as f:
+    while True:
+        char = f.read(1)
+        if not char:
+            break
+        char_list.append(char)
+
+last3 = []
+for i, c in enumerate(char_list):
+    if len(last3) < 3:
+        last3.append(c)
+        continue
+    last3.append(c)
+    len_set = len(set(last3))
+    if len_set == 4:
+        print(i+1)
+        break
+    last3.pop(0)
