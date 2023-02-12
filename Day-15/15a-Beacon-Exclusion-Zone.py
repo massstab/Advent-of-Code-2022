80a7a2067e89cf3361ec455cd2c2a92a2905a1a4
massstab <dave@xiala.net>
2023-02-12 12:59:03 +0100

    Day 15

---
+++ Day-15/15a-Beacon-Exclusion-Zone.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Name:      massstab
+   Date:      07.12.22
+   Kurs:      Advent of Code
+   Topic:     Beacon Exclusion Zone a
+"""
+
+
+
+
+if __name__ == '__main__':
+    with open("data/input.txt") as f:
+        data = f.read().splitlines()
+
+print(data)
